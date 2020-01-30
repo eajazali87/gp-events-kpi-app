4599aa1889b7b353b3b315625129ccd82e664ace
emahaboobbasha <eajazali.mahaboobbasha@sendgrid.com>
2020-01-30 14:56:38 -0700

    test commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Alerts from './components/alerts';
 
 class App extends Component {
   state = {
-    alerts: [],
-    pdutys: []
+    alerts: []
   }
 
   componentDidMount() {
     const options = {
-      headers: new Headers({'content-type': 'application/json','Authorization':'Token token=XXXXXXX'}),
+      headers: new Headers({'content-type': 'application/json','Authorization':'Token token=tTYpWTiYQymaL-Xwzo3H'}),
   };
-    fetch('http://jsonplaceholder.typicode.com/users', options)
+    fetch('https://api.pagerduty.com/teams/PS42LN4', options)
     .then(res => res.json())
     .then((data) => {
       this.setState({ alerts: data })
+      console.log({alerts: data})
     })
     .catch(console.log)
   }
